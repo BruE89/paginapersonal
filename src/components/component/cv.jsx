7a53c8eadcd258170b76c9825db8b336932295db
Bruno <bru_aba89@hotmail.com>
2025-09-11 01:06:12 -0300

    Actualizacion de CVs y mejoras críticas de seguridad

--- src/components/component/cv.jsx
+++ src/components/component/cv.jsx
@@ -2,8 +2,8 @@ const DownloadCV = ({ isSpanish }) => {
   return (
     <a
       href={isSpanish ? 
-        "https://drive.google.com/uc?export=download&id=1WI18mx_rhQut61gB66g9nAIBDSI2ersA" :
-        "https://drive.google.com/uc?export=download&id=1a5Evhr7SlIlkqeVkMyZ3GsEz29DfvW3R"
+        "https://drive.google.com/uc?export=download&id=1COV55mAz7M2Nmf1BTFekXEmqQ_wxGbQl" :
+        "https://drive.google.com/uc?export=download&id=1yr6WrFVZC8ENQ8_WVswdbji_oDq2bzMV"
       }
       download="CV.pdf"
       className="inline-flex h-10 items-center justify-center rounded-md bg-[#4b6a9b] px-6 text-sm font-medium text-white transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-gradient-to-r from-[#4b6a9b] to-[#294a6b] shadow-md hover:from-[#0b2845] hover:to-[#0b2845]"
